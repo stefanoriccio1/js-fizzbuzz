46bfbad489b656ff7d70fe0e3e0409e0c0fa36e2
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-09 16:31:39 +0100

    risultato finale

--- js/script.js
+++ js/script.js
@@ -1,29 +1,75 @@
 // alert ('ciao');
 
 
+// for (var i=1; i <= 100; i++){
+//  // console.log(i);
+//
+//  if (i % 3 != 0 && i % 5 != 0){
+//    console.log(i);
+//  }
+//  else if (i % 5 == 0) {
+//    console.log('Buzz');
+//  }
+//  else if (i % 15 == 0 && i % 3 !=0 && i % 5 !=0){
+//    console.log('FizzBuzz');
+//  }
+//  else if((i % 3 == 0) && (i % 5 == 0) && (i % 15 == 0)){
+//    console.log('Fizz' + ' ' + 'Buzz' + ' ' + 'FizzBuzz');
+//  }
+//  else if ((i % 3 == 0) && (i % 5 == 0)){
+//    console.log('Fizz' +' ' + 'Buzz');
+//  }
+//  else if ((i % 3 == 0) && (i % 15 == 0)){
+//    console.log('Fizz' + ' ' + 'FizzBuzz');
+//  }
+//  else if ((i % 5 == 0) && (i % 15 == 0)) {
+//    console.log('Buzz' + '' + 'FizzBuzz');
+//  }
+//  else{
+//    console.log(i);
+//  }
+// }
+
+// while
+
+// var i=1;
+// while (i<5){
+//   if (i % 3 == 0){
+//     console.log('Fizz');
+//   }
+//   else if (i % 5 == 0) {
+//     console.log('Buzz');
+//   }
+//   else if (i % 15 == 0){
+//     console.log('FizzBuzz');
+//   }
+//   else if(i % 3 == 0 && i % 5 == 0 && i % 15 == 0){
+//     console.log('Fizz' + ' ' + 'Buzz' + ' ' + 'FizzBuzz');
+//   }
+//   else if (i % 3 == 0 && i % 5 == 0){
+//     console.log('Fizz' +' ' + 'Buzz');
+//   }
+//   else if (i % 3 == 0 && i % 15 == 0){
+//     console.log('Fizz' + ' ' + 'FizzBuzz');
+//   }
+//   else if (i % 5 == 0 && i% 15 == 0) {
+//     console.log('Buzz' + '' + 'FizzBuzz');
+//   }
+//   else{
+//     console.log(i);
+//   }
+//   i++;
+// }
 for (var i=1; i <= 100; i++){
  // console.log(i);
-
- if (i % 3 == 0){
-   console.log('Fizz');
- }
- else if (i % 5 == 0) {
-   console.log('Buzz');
- }
- else if (i % 15 == 0){
+ if (i % 15 == 0){
    console.log('FizzBuzz');
  }
- else if(i % 3 == 0 && i % 5 == 0 && i % 15 == 0){
-   console.log('Fizz' + ' ' + 'Buzz' + ' ' + 'FizzBuzz');
+ if (i % 5 ==0 && i%15 != 0){
+   console.log('Buzz');
  }
- else if (i % 3 == 0 && i % 5 == 0){
-   console.log('Fizz' +' ' + 'Buzz');
- }
- else if (i % 3 == 0 && i % 15 == 0){
-   console.log('Fizz' + ' ' + 'FizzBuzz');
- }
- else if (i % 5 == 0 && i% 15 == 0) {
-   console.log('Buzz' + '' + 'FizzBuzz');
+ if (i % 3 ==0 && i%15 != 0){
+   console.log('Fizz');
  }
  else{
    console.log(i);
