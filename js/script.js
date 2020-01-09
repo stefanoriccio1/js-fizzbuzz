ccd42977cf6496b645c840c466eff9ec190b1ad8
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-09 17:27:02 +0100

    stampati su HTML

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ for (var i=1; i <= 100; i++){
 
  if (i % 15 == 0){
    console.log('FizzBuzz');
+   document.getElementById('listaNumeri_quindici').innerHTML += ('<li>' + i + ' FizzBuzz ' + '</li>');
  }
  else if (i % 5 == 0){
    console.log('Buzz');
+   document.getElementById('listaNumeri_cinque').innerHTML += ('<li>' + i + ' Buzz ' + '</li>');
  }
  else if (i % 3 == 0){
    console.log('Fizz');
+   document.getElementById('listaNumeri_tre').innerHTML += ('<li>' + i + ' Fizz ' + '</li>');
  }
  else{
    console.log(i);
+   // document.getElementById('altri').innerHTML += ('<li>' + i + '</li>');
  }
 }
 
