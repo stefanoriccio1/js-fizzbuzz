a1ae99307c49f81113d138eea07725022ca8def2
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-09 15:23:58 +0100

    primo tentativo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 // alert ('ciao');
 
 
-
 for (var i=1; i <= 1000; i++){
   console.log(i);
+
+if (i % 3){
+  console.log('Fizz');
+}
+else if (i % 5) {
+  console.log('Buzz');
+}
+else if (i % 15){
+  console.log('FizzBuzz');
+}
+else{
+  console.log(i);
+}
 }
