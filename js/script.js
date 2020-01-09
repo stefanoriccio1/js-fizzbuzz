17405a1879379f476823ed2fa78c4e521f687d52
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-09 17:11:12 +0100

    Corretti entrambi esercizi, tolte condizioni superflue

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 // alert ('ciao');
 for (var i=1; i <= 100; i++){
- // console.log(i);
+
  if (i % 15 == 0){
    console.log('FizzBuzz');
  }
- else if (i % 5 == 0 && i%15 != 0){
+ else if (i % 5 == 0){
    console.log('Buzz');
  }
- else if (i % 3 == 0 && i%15 != 0){
+ else if (i % 3 == 0){
    console.log('Fizz');
  }
  else{
    console.log(i);
  }
 }
+
+// var listaNumeri = (i=1; i <= 100; i++);
+// console.log(listaNumeri);
 
 // for (var i=1; i <= 100; i++){
 //  // console.log(i);
